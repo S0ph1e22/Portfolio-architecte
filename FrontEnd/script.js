74e15bacc80c1ef3953e7b7bd3cd2587ee6eac95
Sophie <Sophie@sfr.fr>
2025-02-28 15:22:31 +0100

    modal fermer qd on clique sur la croix, filtre fonctionne

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -197,3 +197,78 @@ document.addEventListener('DOMContentLoaded', async function() {
         await fetchProjects();     //att que les projets soit générer
 });
 
+//fenetre ajout
+
+let modal = null
+const focusableSelector = "button, a, input, textarea"
+let focusables = []
+
+const openModal = function(e){
+    e.preventDefault();
+    modal = document.querySelector (e.target.getAttribute ('href'));
+    focusables = Array.from(modal.querySelectorAll(focusableSelector));
+    modal.style.display = null;
+    modal.removeAttribute ('aria-hidden');
+    modal.setAttribute ('aria-modal', 'true');
+    modal.querySelector('.js-modal-close') .addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop') .addEventListener('click', stopPropagation);
+
+    //avoir la mini gallery
+
+    fetchProjects();
+    const gallery = document.querySelector(".gallery");
+    const modalWrapper = modal.querySelector(".modal-wrapper");
+
+    if (gallery && modalWrapper) {
+        const existingClone = modalWrapper.querySelector(".gallery-modal"); 
+        if (!existingClone) { 
+            const galleryClone = gallery.cloneNode(true);
+            galleryClone.classList.add("gallery-modal");
+            modalWrapper.appendChild(galleryClone);
+        }
+    }
+};
+
+const closeModal = function (e){
+    if (modal ===null) return
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute ('aria-hidden', "true");
+    modal.removeAttribute ('aria-modal');
+    modal.removeEventListener ('click', closeModal);
+    modal.querySelector('.js-modal-close') .removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop') .removeEventListener('click', stopPropagation);
+    const galleryClone = modal.querySelector (".gallery-modal");
+    if (galleryClone){
+        galleryClone.remove();
+    }
+    modal = null;
+}
+
+//empeche que qd on clique a l'intérieur le modal
+const stopPropagation = function (e){
+    e.stopPropagation();
+}
+
+const focusInModal = function (e){
+    e.preventDefault();
+    let index = focusables.findIndex( f => f === modal.querySelector(':focus'));
+    index++
+    if (index >= focusables.length){
+        index=0
+    }
+    focusables[index].focus()
+}
+
+document.querySelectorAll('.js-modal').forEach (a =>{
+    a.addEventListener ('click', openModal);
+})
+
+window.addEventListener ('keydown', function(e){
+    if (e.key === "Escape" || e.key === 'Esc'){
+        closeModal(e);
+    }
+    if (e.key=== 'Tab' && modal !== null){
+        focusInModal (e);
+    }
+})
